1455c4d0cb5b9bf91dbfa484a7f13c8f8b55aa6b
Boris <boris1488ss@gmail.com>
2021-08-26 10:50:47 +0300

    almost done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,4 +18,83 @@ $(document).ready(function () {
     });
   }
 
+  const mediaQuery = window.matchMedia('(max-width: 767px)');
+  if (mediaQuery.matches ) {
+    $('.has-submenu').click(function () {
+      $(this).find('.submenu').slideToggle(250);
+    });
+    $('.menu__link').click(function (e) {
+      e.stopPropagation();
+    });
+    $('.header-mobile__bar').click(function () {
+      $('body').toggleClass('ovh');
+      $('.header').toggleClass('header_opened');
+    });
+    $('.footer__heading').click(function () {
+      $(this).siblings('.footer-menu').slideToggle(250);
+    });
+  }
+
+  if($('.filter').length > 0){
+    $('.filter-category__top').click(function () {
+      if($(this).closest('.filter-category').hasClass('filter-category_opened')){
+        $(this).closest('.filter-category').removeClass('filter-category_opened');
+        $(this).siblings('.filter-category__list').slideUp();
+      }else {
+        $(this).closest('.filter-category').addClass('filter-category_opened');
+        $(this).siblings('.filter-category__list').slideDown();
+      }
+    });
+  }
+  if($('.vacancies-sort').length > 0){
+    $('.vacancies-sort').click(function () {
+      $(this).toggleClass('vacancies-sort_active');
+      $('.vacancies-sort__list').slideToggle(250);
+    });
+  }
+
+  if($('.car').length > 0){
+    $('.car-media-big').slick({
+      dots: false,
+      arrows: false,
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.car-media-small',
+      responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+    $('.car-media-small').slick({
+      dots: false,
+      arrows: false,
+      infinite: false,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.car-media-big',
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    });
+    $('.car-tabs-controls__link').click(function (e) {
+      e.preventDefault();
+      var tab = $(this).attr('href');
+      $('.car-tabs-controls__link').removeClass('car-tabs-controls__link_active');
+      $(this).addClass('car-tabs-controls__link_active');
+      $('.car-tab').hide();
+      $('.car-tab'+tab).show();
+    });
+  }
+
 })
